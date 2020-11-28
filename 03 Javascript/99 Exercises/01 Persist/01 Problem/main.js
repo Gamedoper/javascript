99fea2205f4b531e474c5cda7a0beb5acb5040d0
Gamedoper <34165580+Gamedoper@users.noreply.github.com>
2020-11-28 23:27:28 +0530

    Add files via upload

---
+++ 03 Javascript/99 Exercises/01 Persist/01 Problem/main.js
@@ -0,0 +1,11 @@
+var onSave = function(){
+}
+
+var onClear = function(){
+}
+
+window.onload = function(){
+	if (console)C = console.info.bind(console);elseC = function(){};
+	
+	C('hello world!');
+}
---
+++ 03 Javascript/99 Exercises/02 Notes/02 Solution/main.js
@@ -0,0 +1,58 @@
+var onEdit = function(b){
+	var ans = prompt('Change', b.parentElement.textContent);
+
+	if (ans.trim() != ''){
+		b.parentElement.children[0].textContent = ans;
+	}
+}
+
+var onDelete = function(b){
+	C('onDelete', arguments, this, b);
+	var ans = confirm('Are you sure you want to delete "' + b.parentElement.children[0].textContent + '"?');
+
+	if (ans){
+		b.parentElement.remove();
+	}
+}
+
+var onSave = function(){
+	var notefld = document.getElementById('field')
+		,note = notefld.value
+		// ,notehtml = '<li>' + note + '</li>'
+		,notehtml = '<li><span>' + note + '</span><button onclick="onEdit(this)">Edit</button><button onclick="onDelete(this)">Delete</button>' + '</li>'
+		,allnotes = document.getElementById('allnotes')
+	;
+
+	allnotes.innerHTML = allnotes.innerHTML + notehtml;
+
+	var all = [].slice.call(document.getElementById('allnotes').querySelectorAll('span')).map(function(el){
+		return el.textContent;
+	});
+	localStorage.setItem('storednotes', JSON.stringify(all));
+
+	notefld.value = '';
+}
+
+var onClear = function(){
+	localStorage.removeItem('storednotes');
+	allnotes.innerHTML = '';
+}
+
+window.onload = function(){
+	C = console.info.bind(console);
+
+	var allnotes = document.getElementById('allnotes')
+		,all
+		,allnoteshtml
+		,notes = localStorage.getItem('storednotes')
+	;
+	if (notes){
+		all = JSON.parse(notes);
+		all.forEach(function(note){
+			allnoteshtml += '<li><span>' + note + '</span><button onclick="onEdit(this)">Edit</button><button onclick="onDelete(this)">Delete</button>' + '</li>'
+		})
+
+		// allnotes.innerHTML = localStorage.getItem('storednotes');
+		allnotes.innerHTML = allnoteshtml;
+	}
+}
